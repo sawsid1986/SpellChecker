2e2f50d1ca084cae6dbfcf4cab6f06492bfed942
Siddhesh Sawant <siddheshkumar@gmail.com>
2019-04-07 16:18:31 +1000

    Server Changes

--- SpellCheckerWebApp/server.js
+++ SpellCheckerWebApp/server.js
@@ -1,10 +1,7 @@
-'use strict';
-var http = require('http');
-var port = process.env.PORT || 1337;
-
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello Siddhesh\n' + res.connection.localPort);
-}).listen(port);
-
-console.log("App listening on port " + port);
+var express = require('express');
+var app = express();
+app.use(express.static('dist/SpellCheckerApp')); // myApp will be the same folder name.
+app.get('/', function (req, res,next) {
+ res.redirect('/'); 
+});
+app.listen(8080, 'localhost');
